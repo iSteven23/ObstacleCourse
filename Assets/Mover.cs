84e02ea5b4d8ada992378c48461919bfbf0cf79f
Steven Sanchez <stevesanme@gmail.com>
2023-11-29 19:51:58 -0800

    Added rotation
    Added rotation during movement

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -7,11 +7,13 @@ public class Mover : MonoBehaviour
     
     float yValue =  0.00f;
     float moveSpeed = 10.0f;
+    float rotationSpeed = 800.0f; // Adjust this value to control rotation speed
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
         
     }
 
@@ -20,7 +22,27 @@ public class Mover : MonoBehaviour
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue,yValue,zValue);
         
+        Vector3 moveDirection = new Vector3(xValue, 0, zValue);
+
+        // Move the object
+        transform.Translate(moveDirection, Space.World);
+
+        // Check if there is some movement to avoid unnecessary rotation
+        if (moveDirection != Vector3.zero)
+        {
+            // Rotate the object to face the direction of movement
+            Debug.Log("Rotating player");
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the Game.");
+        Debug.Log("Use 'WASD' or arrow keys to move.");
+        Debug.Log("Don't hit the walls.");
     }
 }
