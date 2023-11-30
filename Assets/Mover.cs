874566faa020b72e155d53e994f1eef400044bc3
Steven Sanchez <stevesanme@gmail.com>
2023-11-29 20:24:56 -0800

    last commit for today

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    
-    float yValue =  0.00f;
     float moveSpeed = 10.0f;
-    float rotationSpeed = 800.0f; // Adjust this value to control rotation speed
-
+    float rotationSpeed = 800.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         PrintInstructions();
-        
     }
 
     // Update is called once per frame
@@ -22,27 +18,27 @@ public class Mover : MonoBehaviour
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        
+
         Vector3 moveDirection = new Vector3(xValue, 0, zValue);
 
         // Move the object
         transform.Translate(moveDirection, Space.World);
 
-        // Check if there is some movement to avoid unnecessary rotation
+        // Rotate the object to face the direction of movement
         if (moveDirection != Vector3.zero)
         {
-            // Rotate the object to face the direction of movement
-            Debug.Log("Rotating player");
             Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
-        
+
     }
+
 
     void PrintInstructions()
     {
         Debug.Log("Welcome to the Game.");
         Debug.Log("Use 'WASD' or arrow keys to move.");
+        Debug.Log("Press Space to jump.");
         Debug.Log("Don't hit the walls.");
     }
 }
