a67b746f3e8912f7274589ae9e8bf0628663f267
Steven Sanchez <stevesanme@gmail.com>
2024-01-08 20:28:32 -0800

    Added a scroll view for viewing available json files

---
+++ Assets/Scripts/FileLoader.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+
+public class FileLoader : MonoBehaviour
+{
+    public GameObject scrollViewContent;
+    public GameObject buttonTemplate;
+    public string directoryPath = @"J:\Projects\JSON";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Directory.Exists(directoryPath)){
+            // Get all JSON files in the directory
+            string[] jsonFiles = Directory.GetFiles(directoryPath, "*.json");
+
+            // Loop through each file and print its name
+            foreach (string file in jsonFiles)
+            {
+                string fileName = Path.GetFileName(file);
+                Debug.Log("Found JSON File: " + fileName);
+                GameObject newItems = Instantiate<GameObject>(buttonTemplate, transform);                
+                newItems.transform.SetParent(scrollViewContent.transform);
+                
+                Text buttonText = newItems.GetComponentInChildren<Text>();
+                if (buttonText != null) {
+                    buttonText.text = fileName;
+                }
+            }
+            
+        }
+        else
+        {
+            Debug.LogError("Directory does not exist. ");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
